b4b5e610434cf5a80577fea768bae92e2ffce17e
Sparsh Jaiswal <sparshjaiswal97@gmail.com>
2022-10-29 13:34:56 -0400

    #2.3 Vector for Visualization

--- app/main.cpp
+++ app/main.cpp
@@ -15,9 +15,30 @@ int main()
     
     std::vector<std::vector<double>> plan = Planner.plan();
     std::vector<std::vector<double>> eef_pos;
+    std::vector<std::vector<double>> all_joints_pos;
+    std::vector<double> all_joints_step;
+    // std::vector<double> eef_step;
+
+    double x,y;
     
     for(int i = 0; i<10;i++){
-        eef_pos.emplace_back(twoD.FK(plan[i]));
+
+        auto eef_step = twoD.FK(plan[i]);
+        eef_pos.emplace_back(eef_step);
+        x = links[0]* cos(plan[i][0]);
+        y = links[0]* sin(plan[i][0]);
+
+        all_joints_step.clear();
+        all_joints_step.emplace_back(x);
+        all_joints_step.emplace_back(y);
+        all_joints_step.emplace_back(eef_step[0]);
+        all_joints_step.emplace_back(eef_step[1]);
+
+        all_joints_pos.emplace_back(all_joints_step);
+
+
+
+
 
     }
 
